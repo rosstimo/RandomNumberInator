28dbba3680ea53aeaac2e0e884903c0606d5574e
rosstimo <rosstimo@isu.edu>
2021-02-05 08:26:49 -0700

    fix poor randomization of min and max

--- RandomNumberInator.vb
+++ RandomNumberInator.vb
@@ -1,18 +1,44 @@
 ﻿Module RandomNumberInator
 
     Sub Main()
-        For i = 0 To 10
-            Console.WriteLine(GetRandomNumber(5))
+        Dim currentNumber As Integer
+        Dim randomNumbers(5) As Integer
+
+        For i = 0 To 100000
+            currentNumber = GetRandomNumber(5)
+            Select Case currentNumber
+                Case 0
+                    randomNumbers(0) += 1
+                Case 1
+                    randomNumbers(1) += 1
+                Case 2
+                    randomNumbers(2) += 1
+                Case 3
+                    randomNumbers(3) += 1
+                Case 4
+                    randomNumbers(4) += 1
+                Case 5
+                    randomNumbers(5) += 1
+                Case Else
+                    MsgBox($"Hold on now.. got a {currentNumber}!", vbCritical)
+            End Select
         Next
+        Console.WriteLine(StrDup(39, "-"))
+
+        For i = 0 To UBound(randomNumbers)
+            Console.Write(CStr(randomNumbers(i)).PadLeft(6))
+        Next
+
+        Console.Read()
+
     End Sub
 
-    Function GetRandomNumber(maxNumber As Integer) As Integer
-        Dim randomNumber As Integer
-
+    Function GetRandomNumber(maxNumber As Integer) As Single 'Integer
+        'Dim randomNumber As Integer
+        Dim randomNumber As Single
         Randomize(DateTime.Now.Millisecond)
-
-        randomNumber = CInt(Rnd() * maxNumber)
-
+        'randomNumber = CInt(Rnd() * maxNumber)
+        randomNumber = Rnd() * maxNumber
         Return randomNumber
 
     End Function
