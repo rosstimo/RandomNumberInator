146f331c54e805c2670b1ebabe755debfcd0bdd9
rosstimo <rosstimo@isu.edu>
2021-02-05 08:09:49 -0700

    Add project files.

---
+++ RandomNumberInator.vb
@@ -0,0 +1,20 @@
+﻿Module RandomNumberInator
+
+    Sub Main()
+        For i = 0 To 10
+            Console.WriteLine(GetRandomNumber(5))
+        Next
+    End Sub
+
+    Function GetRandomNumber(maxNumber As Integer) As Integer
+        Dim randomNumber As Integer
+
+        Randomize(DateTime.Now.Millisecond)
+
+        randomNumber = CInt(Rnd() * maxNumber)
+
+        Return randomNumber
+
+    End Function
+
+End Module
