385ff2a870fc90712d1f036e945a33652903d822
rosstimo <rosstimo@isu.edu>
2021-02-18 08:34:23 -0700

    add comments

--- RandomNumberInator.vb
+++ RandomNumberInator.vb
@@ -7,18 +7,21 @@ Module RandomNumberInator
         Dim randomNumbers(5) As Integer
         Dim someArrayOfThings(4, 8) As Boolean
         Dim fudge As Integer
-
+        'load random numbers into array
         For i = 0 To 10000
             randomNumbers(GetRandomNumber(5)) += 1
         Next
+        'Show header info
         fudge = CInt((42 - 14) / 2)
         Console.WriteLine(StrDup(fudge, " ") & "Random Numbers" & StrDup(fudge, " "))
         Console.WriteLine(StrDup(42, "-"))
+        'show column headers
         For i = 0 To 5 Step 1
             Console.Write(Str(i).PadLeft(6) & "|")
         Next
         Console.WriteLine()
         Console.WriteLine(StrDup(42, "-"))
+        'show hit count of random numbers in columns
         For i = 0 To UBound(randomNumbers)
             Console.Write(CStr(randomNumbers(i)).PadLeft(6) & "|")
         Next
